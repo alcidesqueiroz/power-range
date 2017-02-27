abae642599a62e2a701a29ef47fc4d41eda0a80a
Alcides Queiroz <alcidesqueiroz@gmail.com>
2017-02-27 11:07:03 -0300

    Making the package available publicly

---
+++ index.js
@@ -0,0 +1,217 @@
+'use strict';
+
+const typeDetect = require('type-detect');
+
+// A wrapper for the typeDetect function
+const type = val => typeDetect(val).toLowerCase();
+
+// Accepted data types for range limit values
+const validTypes = ['number', 'date', 'string'];
+
+// Possible chars for string ranges
+const numbers = '0123456789';
+const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerCaseLetters = upperCaseLetters.toLowerCase();
+
+function limitsHaveValidType(from, to) {
+  return [from, to].every(val => validTypes.indexOf(type(val)) > -1);
+}
+
+function limitsHaveValidValues(from, to, options) {
+  const rangeType = type(from);
+  let valid;
+
+  if (rangeType === 'number') {
+    valid = !isNaN(from) && !isNaN(to) &&
+      isFinite(from) && isFinite(to);
+  } else if (rangeType === 'date') {
+    valid = !isNaN(from.getTime()) && !isNaN(to.getTime());
+  } else {
+    valid = (from + to)
+      .split('').every(char => options.chars.indexOf(char) > -1);
+  }
+
+  return valid;
+}
+
+function limitsHaveSameType(from, to) {
+  return type(from) === type(to);
+}
+
+function limitsAreInTheRightOrder(from, to, options) {
+  const rangeType = type(from);
+
+  if (rangeType === 'number' || rangeType === 'date') {
+    return to >= from;
+  }
+
+  // So, rangeType is 'string'
+  if (to.length > from.length) return true;
+  if (to.length < from.length) return false;
+
+  const chars = options.chars;
+  for (let i = 0; i < from.length; i++) {
+    if (chars.indexOf(to[i]) > chars.indexOf(from[i])) return true;
+    if (chars.indexOf(to[i]) < chars.indexOf(from[i])) return false;
+  }
+
+  return true;
+}
+
+function validateLimits(from, to, options) {
+  if (!limitsHaveValidType(from, to)) {
+    throw new Error('A range limit value must be a number, date or string.');
+  }
+
+  if (!limitsHaveSameType(from, to)) {
+    throw new Error('Both limit values of a range must have the same type.');
+  }
+
+  if (!limitsHaveValidValues(from, to, options)) {
+    throw new Error('Invalid limit values.');
+  }
+
+  if (!limitsAreInTheRightOrder(from, to, options)) {
+    throw new Error('The passed limit values must be in the right order.');
+  }
+}
+
+function createRangeOfNumbers(from, to, options) {
+  const range = [];
+  const increment = options.increment || 1;
+
+  for (let i = from; i <= to; i += increment) {
+    range.push(i);
+  }
+
+  return range;
+}
+
+function getPossibleChars(from, to, options) {
+  let chars = '';
+
+  if (options.numbers || /\d/.test(from + to)) {
+    chars += numbers;
+  }
+
+  if (options.upperCaseLetters || /[A-Z]/.test(from + to)) {
+    chars += upperCaseLetters;
+  }
+
+  if (options.lowerCaseLetters || /[a-z]/.test(from + to)) {
+    chars += lowerCaseLetters;
+  }
+
+  return chars;
+}
+
+function getNextString(value, chars) {
+  const lastPossibleChar = chars[chars.length - 1];
+  const nextString = value.split('');
+  let incrementNext = true;
+
+  for (let i = nextString.length - 1; i >= 0; i--) {
+    if (!incrementNext) continue;
+
+    if (nextString[i] !== lastPossibleChar) {
+      nextString[i] = chars[chars.indexOf(nextString[i]) + 1];
+      incrementNext = false;
+      continue;
+    }
+
+    nextString[i] = chars[0];
+    if (i === 0) {
+      nextString.unshift(chars[0]);
+    } else {
+      incrementNext = true;
+    }
+  }
+
+  return nextString.join('');
+}
+
+function createRangeOfStrings(from, to, options) {
+  let nextString = from;
+  const range = [nextString];
+
+  while (nextString !== to) {
+    nextString = getNextString(nextString, options.chars);
+    range.push(nextString);
+  }
+
+  return range;
+}
+
+function dateAddMonths(date, increment) {
+  const value = new Date(date.valueOf());
+  let month = date.getMonth();
+  let year = date.getFullYear();
+  month = (month + increment) % 12;
+
+  if (month < 0) {
+    year += (date.getMonth() + increment - month - 12) / 12;
+    month += 12;
+  } else {
+    year += ((date.getMonth() + increment - month) / 12);
+  }
+
+  value.setMonth(month);
+  value.setYear(year);
+
+  return value;
+}
+
+function dateAdd(date, increment, unit) {
+  if (unit === 'month') {
+    return dateAddMonths(date, increment);
+  }
+
+  const factor = {
+    second: 1000,
+    minute: 60000,
+    hour: 3600000,
+    day: 86400000,
+    week: 604800000,
+    year: 31536000000
+  }[unit];
+  const value = date.valueOf() + (factor * increment);
+
+  return new Date(value);
+}
+
+function createRangeOfDates(from, to, options) {
+  let nextDate = new Date(from.getTime());
+  const range = [];
+  const increment = options.increment || 1;
+  const unit = options.unit || 'day';
+
+  while (nextDate <= to) {
+    range.push(nextDate);
+    nextDate = dateAdd(nextDate, increment, unit);
+  }
+
+  return range;
+}
+
+
+
+module.exports = {
+  create(from, to, options) {
+    const rangeType = type(from);
+    const opts = Object.assign({}, options);
+
+    if (rangeType === 'string') {
+      opts.chars = opts.chars || getPossibleChars(from, to, opts);
+    }
+
+    validateLimits(from, to, opts);
+
+    const rangeBuilder = {
+      number: createRangeOfNumbers,
+      string: createRangeOfStrings,
+      date: createRangeOfDates
+    }[rangeType];
+
+    return rangeBuilder(from, to, opts);
+  }
+};
---
+++ test.js
@@ -0,0 +1,226 @@
+const tap = require('tap');
+const pr = require('./index');
+
+/**
+ * Ranges of numbers
+ */
+tap.deepEqual(pr.create(1, 5), [1, 2, 3, 4, 5]);
+tap.deepEqual(pr.create(100, 100), [100]);
+tap.deepEqual(pr.create(99999, 100000), [99999, 100000]);
+tap.deepEqual(pr.create(-3, 3), [-3, -2, -1, 0, 1, 2, 3]);
+tap.deepEqual(pr.create(-7, -2), [-7, -6, -5, -4, -3, -2]);
+tap.deepEqual(pr.create(0, 3), [0, 1, 2, 3]);
+
+/**
+ * Ranges of strings
+ */
+const numbers = '0123456789'.split('');
+const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+tap.deepEqual(pr.create('A', 'G'), ['A', 'B', 'C', 'D', 'E', 'F', 'G']);
+tap.deepEqual(pr.create('X', 'd'), ['X', 'Y', 'Z', 'a', 'b', 'c', 'd']);
+tap.deepEqual(pr.create('DA', 'DD'), ['DA', 'DB', 'DC', 'DD']);
+tap.deepEqual(pr.create('AZ', 'BA'), ['AZ', 'BA']);
+tap.deepEqual(pr.create('6', 'B'), ['6', '7', '8', '9', 'A', 'B']);
+tap.deepEqual(pr.create('6', 'b'), ['6', '7', '8', '9', 'a', 'b']);
+tap.deepEqual(pr.create('z', 'ab'), ['z', 'aa', 'ab']);
+tap.deepEqual(pr.create('ZY', 'AAB'), ['ZY', 'ZZ', 'AAA', 'AAB']);
+tap.deepEqual(pr.create('98', '002'), ['98', '99', '000', '001', '002']);
+
+// Options
+tap.deepEqual(pr.create('AZ', 'BA', { numbers: true }),
+  ['AZ', 'B0', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'BA']);
+tap.deepEqual(pr.create('6', 'b', { upperCaseLetters: true }),
+  ['6', '7', '8', '9'].concat(upperCaseLetters, ['a', 'b']));
+tap.deepEqual(pr.create('Z', 'AB', { lowerCaseLetters: true }),
+  ['Z'].concat(lowerCaseLetters, ['AA', 'AB']));
+tap.deepEqual(pr.create('$', '%#', { chars: '$%#' }),
+  ['$', '%', '#', '$$', '$%', '$#', '%$', '%%', '%#']);
+
+
+/**
+ * Ranges of dates
+ */
+tap.deepEqual(pr.create(new Date(2001, 2, 27, 4, 5, 6), new Date(2001, 3, 5, 6, 7, 8)),
+  [
+    new Date(2001, 2, 27, 4, 5, 6),
+    new Date(2001, 2, 28, 4, 5, 6),
+    new Date(2001, 2, 29, 4, 5, 6),
+    new Date(2001, 2, 30, 4, 5, 6),
+    new Date(2001, 2, 31, 4, 5, 6),
+    new Date(2001, 3, 1, 4, 5, 6),
+    new Date(2001, 3, 2, 4, 5, 6),
+    new Date(2001, 3, 3, 4, 5, 6),
+    new Date(2001, 3, 4, 4, 5, 6),
+    new Date(2001, 3, 5, 4, 5, 6)
+  ]);
+
+tap.deepEqual(pr.create(new Date(2001, 2, 3, 4, 5, 6), new Date(2001, 2, 3, 4, 5, 6)),
+  [new Date(2001, 2, 3, 4, 5, 6)]);
+
+// Options
+tap.deepEqual(pr.create(new Date(2001, 2, 3, 4, 5, 6), new Date(2001, 2, 3, 4, 5, 12), { unit: 'second' }),
+  [
+    new Date(2001, 2, 3, 4, 5, 6),
+    new Date(2001, 2, 3, 4, 5, 7),
+    new Date(2001, 2, 3, 4, 5, 8),
+    new Date(2001, 2, 3, 4, 5, 9),
+    new Date(2001, 2, 3, 4, 5, 10),
+    new Date(2001, 2, 3, 4, 5, 11),
+    new Date(2001, 2, 3, 4, 5, 12)
+  ]);
+
+tap.deepEqual(pr.create(new Date(2001, 2, 3, 4, 55, 6), new Date(2001, 2, 3, 5, 2, 6), { unit: 'minute' }),
+  [
+    new Date(2001, 2, 3, 4, 55, 6),
+    new Date(2001, 2, 3, 4, 56, 6),
+    new Date(2001, 2, 3, 4, 57, 6),
+    new Date(2001, 2, 3, 4, 58, 6),
+    new Date(2001, 2, 3, 4, 59, 6),
+    new Date(2001, 2, 3, 5, 0, 6),
+    new Date(2001, 2, 3, 5, 1, 6),
+    new Date(2001, 2, 3, 5, 2, 6)
+  ]);
+
+tap.deepEqual(pr.create(new Date(2001, 2, 3, 17, 5, 6), new Date(2001, 2, 4, 1, 5, 6), { unit: 'hour' }),
+  [
+    new Date(2001, 2, 3, 17, 5, 6),
+    new Date(2001, 2, 3, 18, 5, 6),
+    new Date(2001, 2, 3, 19, 5, 6),
+    new Date(2001, 2, 3, 20, 5, 6),
+    new Date(2001, 2, 3, 21, 5, 6),
+    new Date(2001, 2, 3, 22, 5, 6),
+    new Date(2001, 2, 3, 23, 5, 6),
+    new Date(2001, 2, 4, 0, 5, 6),
+    new Date(2001, 2, 4, 1, 5, 6)
+  ]);
+
+tap.deepEqual(pr.create(new Date(2001, 2, 27, 4, 5, 6), new Date(2001, 3, 5, 6, 7, 8), { unit: 'day' }),
+  [
+    new Date(2001, 2, 27, 4, 5, 6),
+    new Date(2001, 2, 28, 4, 5, 6),
+    new Date(2001, 2, 29, 4, 5, 6),
+    new Date(2001, 2, 30, 4, 5, 6),
+    new Date(2001, 2, 31, 4, 5, 6),
+    new Date(2001, 3, 1, 4, 5, 6),
+    new Date(2001, 3, 2, 4, 5, 6),
+    new Date(2001, 3, 3, 4, 5, 6),
+    new Date(2001, 3, 4, 4, 5, 6),
+    new Date(2001, 3, 5, 4, 5, 6)
+  ]);
+
+tap.deepEqual(pr.create(new Date(2001, 11, 5, 4, 5, 6), new Date(2002, 0, 23, 4, 5, 6), { unit: 'week' }),
+  [
+    new Date(2001, 11, 5, 4, 5, 6),
+    new Date(2001, 11, 12, 4, 5, 6),
+    new Date(2001, 11, 19, 4, 5, 6),
+    new Date(2001, 11, 26, 4, 5, 6),
+    new Date(2002, 0, 2, 4, 5, 6),
+    new Date(2002, 0, 9, 4, 5, 6),
+    new Date(2002, 0, 16, 4, 5, 6),
+    new Date(2002, 0, 23, 4, 5, 6)
+  ]);
+
+tap.deepEqual(pr.create(new Date(2001, 2, 3, 4, 5, 6), new Date(2002, 3, 3, 4, 5, 6), { unit: 'month' }),
+  [
+    new Date(2001, 2, 3, 4, 5, 6),
+    new Date(2001, 3, 3, 4, 5, 6),
+    new Date(2001, 4, 3, 4, 5, 6),
+    new Date(2001, 5, 3, 4, 5, 6),
+    new Date(2001, 6, 3, 4, 5, 6),
+    new Date(2001, 7, 3, 4, 5, 6),
+    new Date(2001, 8, 3, 4, 5, 6),
+    new Date(2001, 9, 3, 4, 5, 6),
+    new Date(2001, 10, 3, 4, 5, 6),
+    new Date(2001, 11, 3, 4, 5, 6),
+    new Date(2002, 0, 3, 4, 5, 6),
+    new Date(2002, 1, 3, 4, 5, 6),
+    new Date(2002, 2, 3, 4, 5, 6),
+    new Date(2002, 3, 3, 4, 5, 6)
+  ]);
+
+// tap.deepEqual(pr.create(new Date(2001, 11, 3, 4, 5, 6), new Date(2007, 11, 3, 4, 5, 7), { unit: 'year' }),
+//   [
+//     new Date(2001, 11, 3, 4, 5, 6),
+//     new Date(2002, 11, 3, 4, 5, 6),
+//     new Date(2003, 11, 3, 4, 5, 6),
+//     new Date(2004, 11, 3, 4, 5, 6),
+//     new Date(2005, 11, 3, 4, 5, 6),
+//     new Date(2006, 11, 3, 4, 5, 6),
+//     new Date(2007, 11, 3, 4, 5, 6)
+//   ]);
+
+tap.deepEqual(pr.create(new Date(2001, 2, 27, 4, 5, 6), new Date(2001, 3, 5, 6, 7, 8), { unit: 'day', increment: 2 }),
+  [
+    new Date(2001, 2, 27, 4, 5, 6),
+    new Date(2001, 2, 29, 4, 5, 6),
+    new Date(2001, 2, 31, 4, 5, 6),
+    new Date(2001, 3, 2, 4, 5, 6),
+    new Date(2001, 3, 4, 4, 5, 6)
+  ]);
+
+tap.deepEqual(pr.create(new Date(2001, 2, 3, 4, 5, 6), new Date(2002, 3, 4, 5, 6, 7), { unit: 'month', increment: 2 }),
+  [
+    new Date(2001, 2, 3, 4, 5, 6),
+    new Date(2001, 4, 3, 4, 5, 6),
+    new Date(2001, 6, 3, 4, 5, 6),
+    new Date(2001, 8, 3, 4, 5, 6),
+    new Date(2001, 10, 3, 4, 5, 6),
+    new Date(2002, 0, 3, 4, 5, 6),
+    new Date(2002, 2, 3, 4, 5, 6)
+  ]);
+
+tap.deepEqual(pr.create(new Date(2001, 2, 27, 4, 5, 6), new Date(2001, 3, 5, 6, 7, 8), { increment: 2 }),
+  [
+    new Date(2001, 2, 27, 4, 5, 6),
+    new Date(2001, 2, 29, 4, 5, 6),
+    new Date(2001, 2, 31, 4, 5, 6),
+    new Date(2001, 3, 2, 4, 5, 6),
+    new Date(2001, 3, 4, 4, 5, 6)
+  ]);
+
+
+/**
+ * Errors
+ */
+
+//Accepted types checking
+tap.throws(() => pr.create(true, 2),
+  new Error('A range limit value must be a number, date or string.'));
+tap.throws(() => pr.create({}, new Date(2001, 3, 3)),
+  new Error('A range limit value must be a number, date or string.'));
+tap.throws(() => pr.create('whatever', []),
+  new Error('A range limit value must be a number, date or string.'));
+tap.throws(() => pr.create(null, 3),
+  new Error('A range limit value must be a number, date or string.'));
+tap.throws(() => pr.create(1, undefined),
+  new Error('A range limit value must be a number, date or string.'));
+
+ //Invalid limit values
+tap.throws(() => pr.create(1, Infinity),
+  new Error('Invalid limit values.'));
+tap.throws(() => pr.create(NaN, 3),
+  new Error('Invalid limit values.'));
+tap.throws(() => pr.create(new Date(999999, 999999, 999999), new Date(2001, 1, 1)),
+  new Error('Invalid limit values.'));
+tap.throws(() => pr.create('abc', '_#@'),
+  new Error('Invalid limit values.'));
+
+//Limits same type checking
+tap.throws(() => pr.create(1, new Date()),
+  new Error('Both limit values of a range must have the same type.'));
+tap.throws(() => pr.create("a", 1),
+  new Error('Both limit values of a range must have the same type.'));
+tap.throws(() => pr.create("a", new Date()),
+  new Error('Both limit values of a range must have the same type.'));
+tap.throws(() => pr.create("1", 2),
+  new Error('Both limit values of a range must have the same type.'));
+
+//Limits order checking
+tap.throws(() => pr.create(3, 2),
+  new Error('The passed limit values must be in the right order.'));
+tap.throws(() => pr.create(new Date(2001, 3, 4), new Date(2001, 3, 3)),
+  new Error('The passed limit values must be in the right order.'));
+tap.throws(() => pr.create('C', '2'),
+  new Error('The passed limit values must be in the right order.'));
